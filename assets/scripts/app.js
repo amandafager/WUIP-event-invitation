31ee6e6f74f5b0d8b1471ba8473e0f5103b7685c
amanda.fager@outlook.com <amanda.fager@outlook.com>
2021-03-09 18:03:13 +0100

    mobile layout update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,5 @@
 
-window.addEventListener(
-  "scroll",
-  () => {
+window.addEventListener("scroll", () => {
     document.body.style.setProperty(
       "--scroll",
       window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
@@ -9,6 +7,39 @@ window.addEventListener(
   },
   false
 );
+
+
+
+(function() {
+  let elements;
+  let windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (let i = 0; i < elements.length; i++) {
+      let element = elements[i];
+      let positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('fade-in-element');
+        element.classList.remove('hidden');
+      }
+    
+    }
+  }
+  
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+})();
+
+
 
 
 let urlParams = new URLSearchParams(window.location.search);
